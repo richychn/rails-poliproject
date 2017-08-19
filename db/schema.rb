eb44dcd9e57b450dba401f3442ea9545743665e6
Richy Chen <richychn@hotmail.com>
2017-08-19 16:42:20 +0100

    created scan model with both references and new/create routes under user for scans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819152501) do
+ActiveRecord::Schema.define(version: 20170819153936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20170819152501) do
     t.boolean  "bias"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "scans", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_scans_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_scans_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +50,6 @@ ActiveRecord::Schema.define(version: 20170819152501) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "scans", "articles"
+  add_foreign_key "scans", "users"
 end
