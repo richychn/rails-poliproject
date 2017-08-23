33c777aeb78eee93d9eb2913e0a61ddd5afdad07
Richy Chen <richychn@hotmail.com>
2017-08-23 12:44:36 +0100

    upload avatar works and shows on navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @last_article = current_user.scans.last.article
+    if current_user.scans.any?
+      @last_article = current_user.scans.last.article
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount Attachinary::Engine => "/attachinary"
   root to: 'pages#home'
   resources :articles, only: [ :create ]
   resources :users, only: [ :show ] do
