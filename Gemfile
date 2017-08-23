3017f8db9bb8a712717cf0494e8b3788fb6d217b
Richy Chen <richychn@hotmail.com>
2017-08-23 14:24:55 +0100

    facebook works with avatar and good button

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'attachinary', github: 'assembler/attachinary'
 gem 'jquery-fileupload-rails'
 gem 'coffee-rails'
 gem 'devise'
+gem 'omniauth-facebook'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def avatar_url
+    if current_user.photo
+      @avatar_url = cl_image_path current_user.photo.path, height: 150, width: 150, crop: :thumb, gravity: :face
+    elsif current_user.facebook_picture_url
+      @avatar_url = current_user.facebook_picture_url
+    else
+      @avatar_url = image_url "logo-blue.png"
+    end
+  end
 end
