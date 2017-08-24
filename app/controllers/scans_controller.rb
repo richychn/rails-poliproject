d486029020ab802c1d27d8362d6b75ff0d42e92a
Richy Chen <richychn@hotmail.com>
2017-08-24 09:45:34 +0100

    added trial defs to scan controller

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
-require "google/cloud/language"
+# require "google/cloud/language"
 
 class ScansController < ApplicationController
 
@@ -22,7 +22,13 @@ class ScansController < ApplicationController
 
   def url_bias(url)
     text = webpage_to_text(url)
-    syntax = text_analysis(text)
+    if scan_text(text) < 0.79
+      return true
+    else
+      return false
+    end
+    # syntax = text_analysis(text)
+
   end
 
   def webpage_to_text(url)
@@ -35,9 +41,35 @@ class ScansController < ApplicationController
     return text
   end
 
-  def text_analysis(text)
-    language  = Google::Cloud::Language.new project: PROJECT_ID
-    document  = language.document text
-    return document.syntax
+  # def text_analysis(text)
+  #   language  = Google::Cloud::Language.new project: PROJECT_ID
+  #   document  = language.document text
+  #   return document.syntax
+  # end
+
+  def scan_text(text)
+    array = text_to_array(text)
+    total = array.length
+    # array = rm_stop_words!(array)
+    nouns = no_of_part_hash2(array, "N")
+    p nouns / total.to_f
+    return nouns / array.length.to_f
+  end
+
+  def text_to_array(text)
+    array = text.downcase.gsub(/[^a-z0-9\s]/i, " ").split
+    return array
+  end
+
+  def no_of_part_hash2(array, part)
+    pool = ThreadPool.new(size: 200)
+    nhash = frequency(array)
+    pool.schedule do
+      nhash.each do |key, value|
+        nhash[key] = 0 unless check(key, part)
+      end
+    end
+    pool.shutdown
+    return nhash.values.inject(&:+)
   end
 end
